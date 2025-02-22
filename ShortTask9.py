321541b5e98aba535f9fe8528134257874e641ab
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-02-23 02:30:34 +0530

    ShortTask9.py

--- ShortTask9.py
+++ ShortTask9.py
@@ -7,4 +7,14 @@ add=0
 for  val in height:
     add=add+val
     
-print('average height is ',add/len(height))   
+print('average height is ',add/len(height))  
+
+#alternate method
+# height=(input("Enter height of students")).split(" ")
+
+# for n in range(0,len(height)):
+#     height[n]=int(height[n])
+
+# a=sum(height)/len(height)
+
+# print(a)
