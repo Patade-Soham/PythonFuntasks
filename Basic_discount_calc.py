f273d3c95d346071e249eb4b82526f600c2a12e7
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-02-26 02:24:45 +0530

    Create Basic_discount_calc.py

---
+++ Basic_discount_calc.py
@@ -0,0 +1,23 @@
+n=input("n1:")
+a=int(n)
+
+print("original price",n)
+
+a=input("item 1:")
+b=input("cost")
+c=input("item 2:")
+d=input("cost")
+e=input("item 3:")
+f=input("cost")
+
+m=int(b)
+n=int(d)
+o=int(f)
+t=m+n+o
+ans=0.9*t
+
+print (a,"  ",m)
+print (c," ",n)
+print (e,"  ",o)
+print ("total cost : ",t)
+print("after discount:",ans,"Rs/-")
