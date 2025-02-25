eea98811f6f9375af870df2cb29ecceac9f58474
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-02-26 02:32:38 +0530

    Create Basic_Arithmetics.py

---
+++ Basic_Arithmetics.py
@@ -0,0 +1,19 @@
+def opp():
+    a=int(input("Enter first integer : "))
+    b=int(input("Enter first integer : "))
+    
+    summ= a+b
+    sub= a-b
+    multi= a*b
+    div = a/b
+    mod= a%b
+    
+    
+    print("Addition   : ",summ)
+    print("Subtraction: ",sub)
+    print("Multiply   : ",multi)
+    print("Division   : ",div)
+    print("Modulus    : ",mod)
+
+
+opp()
