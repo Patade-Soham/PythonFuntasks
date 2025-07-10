2fd575cc618cb2f0d286ec5c797d53b1adadeec2
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2025-07-11 02:42:12 +0530

    Add files via upload

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,39 +1,53 @@
-from turtle import Turtle,Screen
-import random
+from turtle import Screen
 import time
-
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
+screen.title("Snake Game")
+screen.tracer(0)
 
-starting_position = [(0,0),(-20,0),(-40,0)]
-turtles=[]
-for position in starting_position:
-    new_tim=Turtle()
-    new_tim.shape('square')
-    new_tim.color('white')
-    new_tim.penup()
-    new_tim.goto(position)
-    turtles.append(new_tim)
+
+
+scoreboard = Scoreboard()
+snake = Snake()
+food = Food()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
 
 
 game_is_on = True
-
 while game_is_on:
-
     screen.update()
-
     time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        game_is_on = False
+        scoreboard.game_over()
+    for segment in snake.segments:
+        if segment==snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
-    for i in range(len(turtles)- 1, 0, -1):
 
-        new_x = turtles[i].xcor()
 
-        new_y = turtles[i].ycor()
 
-        turtles[i].goto(new_x, new_y)
 
-        turtles[0].forward(28)
+
 
 
 
